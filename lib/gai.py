3f76adaeb672b9a22bbae05a3cffd538d16b5f9c
allthatilk <imogenkutz@hotmail.com>
2017-05-15 19:14:44 +0100

    Setting up folders and pytest paths

---
+++ lib/gai.py
@@ -0,0 +1,21 @@
+"""Script to return Global Adaptation Index (GAI) data by country."""
+import sys
+
+def main(csv):
+    """Main entry point for the script."""
+    pass
+
+def return_data(iso_code):
+    """Takes an ISO code as an argument and returns options for data"""
+    pass
+
+def list_data(country):
+    """Lists data available for given country"""
+    pass
+
+def average_data(country):
+    """Returns an average of all GAI scores for all available years for a given country"""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
