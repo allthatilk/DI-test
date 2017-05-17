d72514ab3eb22bfacd15145df8ea5f4e1c2874f0
allthatilk <imogenkutz@hotmail.com>
2017-05-18 00:01:09 +0100

    Adding explaining strings to run.py

--- run.py
+++ run.py
@@ -2,12 +2,18 @@
 from lib.data import CountryData
 from lib.averages import Averages
 
-country_data = CountryData('lib/gai.csv', "AE")
+print("Global Adaptation Index data for Afghanistan\n")
+
+country_data = CountryData('lib/gai.csv', "AF")
 country_data.createDataset()
+print("List of available GAI scores for available years:")
 print(country_data.listData())
 
 averages = Averages(country_data)
 averages.prepData
+print("\nMean Score:")
 print(averages.mean())
+print("\nScore Range:")
 print(averages.range())
+print("\nStandard Deviation from the Mean:")
 print(averages.standardDeviation())
