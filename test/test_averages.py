d7831c6b1be0fd4236282848bbab8b3e4ea0fb8e
allthatilk <imogenkutz@hotmail.com>
2017-05-22 22:07:27 +0100

    Refactored test_data file to reuse mock method

--- test/test_averages.py
+++ test/test_averages.py
@@ -1,6 +1,11 @@
 from lib.averages import Averages
+from test.test_data import mock
+# import requests
+# import requests_mock
 
-# Averages class initialises
+def test_Averages():
+    averages = Averages(mock())
+    assert averages.country_data == [('2000', '0.3'), ('2001', '0.4'), ('2002', '0.1'), ('2003', '0.2')]
 
 # prepData returns scores in desired formatted
 
--- test/test_data.py
+++ test/test_data.py
@@ -2,7 +2,14 @@ from lib.data import CountryData
 import requests
 import requests_mock
 
-data = "id,year,value\nTE,2000,0.3\nTE,2001,0.4\nTE,2002,0.1\nTE,2003,0.2"
+
+def mock():
+    data = "id,year,value\nTE,2000,0.3\nTE,2001,0.4\nTE,2002,0.1\nTE,2003,0.2"
+    with requests_mock.mock() as m:
+        m.get('http://test.com', text=data)
+        country = CountryData('http://test.com')
+        country.createDataset('TE')
+        return country
 
 def test_CountryData():
     """Test to see that CountryData class can be instantiated"""
@@ -12,16 +19,10 @@ def test_CountryData():
 
 def test_createDataset():
     """Test to see that the dataset builds as expected"""
-    with requests_mock.mock() as m:
-        m.get('http://test.com', text=data)
-        country = CountryData('http://test.com')
-        country.createDataset('TE')
-        assert country.dataset == [('2000', '0.3'), ('2001', '0.4'), ('2002', '0.1'), ('2003', '0.2')]
+    country = mock()
+    assert country.dataset == [('2000', '0.3'), ('2001', '0.4'), ('2002', '0.1'), ('2003', '0.2')]
 
 def test_listData():
     """Test to see that listData returns a list of years and GAI scores for specified ISO code"""
-    with requests_mock.mock() as m:
-        m.get('http://test.com', text=data)
-        country = CountryData('http://test.com')
-        country.createDataset('TE')
-        assert country.listData() == '2000 | 0.3\n2001 | 0.4\n2002 | 0.1\n2003 | 0.2'
+    country = mock()
+    assert country.listData() == '2000 | 0.3\n2001 | 0.4\n2002 | 0.1\n2003 | 0.2'
