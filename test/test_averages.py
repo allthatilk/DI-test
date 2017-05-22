4c3b9a277d05a6c554b689b797bb5482c7e02ffb
allthatilk <imogenkutz@hotmail.com>
2017-05-22 22:15:13 +0100

    Creating test for prepData method of Averages class

--- test/test_averages.py
+++ test/test_averages.py
@@ -1,13 +1,18 @@
 from lib.averages import Averages
 from test.test_data import mock
-# import requests
-# import requests_mock
 
 def test_Averages():
+    """Tests to see that Averages class can be instantiated"""
     averages = Averages(mock())
     assert averages.country_data == [('2000', '0.3'), ('2001', '0.4'), ('2002', '0.1'), ('2003', '0.2')]
 
-# prepData returns scores in desired formatted
+def test_prepData():
+    """Tests to see that prepData returns scores in desired format"""
+    averages = Averages(mock())
+    averages.prepData()
+    assert averages.data == [0.3, 0.4, 0.1, 0.2]
+
+
 
 # mean returns the mean
 
