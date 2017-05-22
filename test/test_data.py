f1ecee97ff75391fcc1ed64fd8ffdf9ba3d51ccd
allthatilk <imogenkutz@hotmail.com>
2017-05-22 15:56:07 +0100

    Implementing test of refactored CountryData class

--- test/test_data.py
+++ test/test_data.py
@@ -1,16 +1,21 @@
 from lib.data import CountryData
+import requests_mock
+
+data = "id,year,value\nTE,2000,0.3\nTE,2001,0.4\nTE,2002,0.1\nTE,2003,0.2"
 
 # need to look into requests.get to figure out how to mock csv data
+def test_CountryData():
+    """Test to see that CountryData class can be instantiated"""
+    url = 'this is a URL'
+    country = CountryData(url)
+    assert country.url == 'this is a URL'
 
-
-# Class initialises
-# def test_CountryData():
-#     # url = "this is a url"
-#     # country = CountryData(url)
-#     assert country == #CountryData type of object
+# def test_createDataset():
+#     adapter = requests_mock.Adapter()
+#     adapter.register_uri('GET', 'mock://test.com/path', text=data)
+#     country = CountryData('mock://test.com/path')
 
 # createDataset returns dataset for given country_data
-# def test_createDataset():
 #
 #     assert data ==
 
