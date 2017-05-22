a16c53dedcbcf74b2eb0d5775bebb200ea3a83bb
allthatilk <imogenkutz@hotmail.com>
2017-05-22 22:17:56 +0100

    Creating test for mean method of Averages class

--- test/test_averages.py
+++ test/test_averages.py
@@ -1,20 +1,26 @@
 from lib.averages import Averages
 from test.test_data import mock
 
+averages = Averages(mock())
+
 def test_Averages():
     """Tests to see that Averages class can be instantiated"""
-    averages = Averages(mock())
     assert averages.country_data == [('2000', '0.3'), ('2001', '0.4'), ('2002', '0.1'), ('2003', '0.2')]
 
 def test_prepData():
     """Tests to see that prepData returns scores in desired format"""
-    averages = Averages(mock())
     averages.prepData()
     assert averages.data == [0.3, 0.4, 0.1, 0.2]
 
+def test_mean():
+    """Tests to see that mean returns the mean"""
+    assert averages.mean() == 0.25
 
 
-# mean returns the mean
+
+
+
+
 
 # range returns the range
 
