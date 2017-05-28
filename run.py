ab2d1f3d5a2ef4bfb0d702422cea2336e9f09836
allthatilk <imogenkutz@hotmail.com>
2017-05-28 10:51:12 +0100

    Adding main function so run.py can be imported without executing

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from lib.data import CountryData
 from lib.averages import Averages
 
-print("Global Adaptation Index data for selected country")
 
 country_data = CountryData("https://raw.githubusercontent.com/devinit/digital-platform/master/country-year/climate-vulnerability.csv")
 
@@ -21,10 +20,13 @@ def chooseCountry(iso_code):
     print("\nStandard Deviation from the Mean:")
     print(averages.standardDeviation())
 
-iso_code = input("Please provide a country's ISO code:\n")
-chooseCountry(iso_code)
-while iso_code != 'quit':
-    iso_code = input("\nType 'quit' to exit, or type another ISO code:\n")
-    chooseCountry(iso_code) if iso_code != 'quit' else quit()
+def main():
+    print("Global Adaptation Index data for selected country")
+    iso_code = input("Please provide a country's ISO code:\n")
+    chooseCountry(iso_code)
+    while iso_code != 'quit':
+        iso_code = input("\nType 'quit' to exit, or type another ISO code:\n")
+        chooseCountry(iso_code) if iso_code != 'quit' else quit()
 
-# Implemented functionality for user to select country as script runs in command line after comment made at interview
+if __name__ == '__main__':
+    main()
